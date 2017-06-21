4c47b032bb97acc569b5a398fd4f6429c24e5f6c
Robin ten Buuren <robin.tenbuuren@nedap.com>
2017-06-21 22:23:50 +0200

    Remove obsolete Zooming Camera and fix panning Camera code

--- Assets/2DTileMapLevelEditor/Scripts/MoveCamera.cs
+++ Assets/2DTileMapLevelEditor/Scripts/MoveCamera.cs
@@ -11,11 +11,9 @@ public class MoveCamera : MonoBehaviour
 	//
 
 	public float panSpeed = 4.0f;		// Speed of the camera when being panned
-	public float zoomSpeed = 4.0f;		// Speed of the camera going back and forth
 
 	private Vector3 mouseOrigin;	// Position of cursor when mouse dragging starts
 	private bool isPanning;		// Is the camera being panned?
-	private bool isZooming;		// Is the camera zooming?
 
 	//
 	// UPDATE
@@ -24,24 +22,15 @@ public class MoveCamera : MonoBehaviour
 	void Update () 
 	{
 		// Get the right mouse button
-		if(Input.GetMouseButtonDown(0))
+		if(Input.GetMouseButtonDown(1))
 		{
 			// Get mouse origin
 			mouseOrigin = Input.mousePosition;
 			isPanning = true;
 		}
 
-		// Get the middle mouse button
-		if(Input.GetMouseButtonDown(1))
-		{
-			// Get mouse origin
-			mouseOrigin = Input.mousePosition;
-			isZooming = true;
-		}
-
 		// Disable movements on button release
 		if (!Input.GetMouseButton(1)) isPanning=false;
-		if (!Input.GetMouseButton(2)) isZooming=false;
 
 		// Move the camera on it's XY plane
 		if (isPanning)
@@ -50,15 +39,6 @@ public class MoveCamera : MonoBehaviour
 
 			Vector3 move = new Vector3(pos.x * panSpeed, pos.y * panSpeed, 0);
 			transform.Translate(move, Space.Self);
-		}
-
-		// Move the camera linearly along Z axis
-		if (isZooming)
-		{
-			Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
-
-			Vector3 move = pos.y * zoomSpeed * transform.forward; 
-			transform.Translate(move, Space.World);
 		}
 	}
 }
